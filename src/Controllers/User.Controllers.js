44f254ff5aa2bae0d0b2611938a9a4c36baf844a
anus26 <anusraza268@gmail.com>
2025-08-28 19:41:39 -0700

    authentciation

--- src/Controllers/User.Controllers.js
+++ src/Controllers/User.Controllers.js
@@ -18,6 +18,7 @@ const signup=async(req,res)=>{
           password,
           confirmpassword
     })
+     await user.save()
    
    } catch (error) {
      console.log(error);
@@ -26,5 +27,54 @@ const signup=async(req,res)=>{
 
 }
 
-export {signup}
+
+const signin=async(req,res)=>{
+    try {
+        const {email,password}=req.body
+        const user=await User.findOne({email})
+
+        if (!user) {
+            return res.status(400).json({error:"user not match"})
+            
+        }
+     if (user.password!==password) {
+return res.status(400).json({error:"password are not match"})
+        
+     }
+
+        return res.status(200).json({user})
+    } catch (error) {
+         console.log(error);
+        res.status(500).json({error:"internal server error"})
+    }
+}
+
+
+const logout=async(req,res)=>{
+    try {
+        const {email}=req.body
+        const user=await User.findOneAndDelete({email})
+        return res.status(200).json({user})
+    } catch (error) {
+             console.log(error);
+        res.status(500).json({error:"internal server error"})
+    }
+}
+
+const alluserid=async(req,res)=>{
+    try {
+        const {id}=req.params
+              const user=await User.findById(id)
+        if (!user) {
+            return res.status(400).json({error:"id not match"})
+            
+        }
+        return res.status(200).json({user})
+    } catch (error) {
+            console.log(error);
+        res.status(500).json({error:"internal server error"}) 
+    }
+}
+
+export {signup,signin,logout,alluserid}
 
