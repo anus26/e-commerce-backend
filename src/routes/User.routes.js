ede3c7779fec5986d3e1b6b504e03e37d8dcf901
anus26 <anusraza268@gmail.com>
2025-08-28 19:41:39 -0700

    authentciation

--- src/routes/User.routes.js
+++ src/routes/User.routes.js
@@ -1,6 +1,9 @@
 import express from "express"
-import { signup } from "../Controllers/User.Controllers.js"
+import { alluserid, logout, signin, signup } from "../Controllers/User.Controllers.js"
 const router=express.Router()
 
 router.post('/signup',signup)
+router.post('/signin',signin)
+router.delete('/logout',logout)
+router.get('/alluserid/:id',alluserid)
 export default  router
