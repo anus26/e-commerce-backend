b58c411d47fa9afbc89cfe2e8daecc430790f426
anus26 <anusraza268@gmail.com>
2025-09-05 00:24:53 -0700

    coustomer

--- src/routes/User.routes.js
+++ src/routes/User.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { alluser, alluserid, forgetpassword, logout, resetpass, resetpassword, sendMailcon, signin, signup, verification } from "../Controllers/User.Controllers.js"
+import { alluser, alluserid, forgetpassword, logout, resendOtp, resetpass, resetpassword, sendMailcon, signin, signup, verification } from "../Controllers/User.Controllers.js"
 import authmiddleware from "../middleware/User.middleware.js"
 const router=express.Router()
 
@@ -13,5 +13,6 @@ router.post('/forget',forgetpassword)
 router.post('/reset-password/:token', resetpassword);
 router.post('/verif',verification)
 router.post('/reset',resetpass)
+router.get('/resend',resendOtp)
 
 export default  router
