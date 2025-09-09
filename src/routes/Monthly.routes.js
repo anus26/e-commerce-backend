6e6eed7f83ea8379ed60978c26992011b7cadad8
anus26 <anusraza268@gmail.com>
2025-09-08 20:09:20 -0700

    static

--- src/routes/Monthly.routes.js
+++ src/routes/Monthly.routes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { getmonth, monthData } from "../Controllers/Monthly.Controllers.js"
+import { getmonth, monthData, onemonth } from "../Controllers/Monthly.Controllers.js"
 
 const monthrouter=express.Router()
 
 monthrouter.post("/month",monthData)
 monthrouter.get("/get",getmonth)
+monthrouter.get("/get/:month",onemonth)
 
 export default monthrouter
