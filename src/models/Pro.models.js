a6e42e8907a94797d8eca443e1f4079f0d25bbc2
anus26 <anusraza268@gmail.com>
2025-09-11 19:11:14 -0700

    models

---
+++ src/models/Pro.models.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const productSchema=new mongoose.Schema({
+    ProductName:{
+        type:String,
+        required:true
+        },
+        Category:{
+            type:String,
+            required:true
+        },
+        Brand:{
+            type:String,
+            required:true
+        },
+        color:{
+            type:String,
+            required:true
+        },
+        Weight:{
+            type:Number,
+            required:true
+        },
+        Length:{
+            type:Number,
+            required:true
+        },
+        Width:{
+            type:Number,
+            required:true
+        },
+        Description:{
+            type:String,
+            required:true
+        },
+        Price:{
+            type:Number,
+            required:true
+        },
+        StockQuantity:{
+            type:String,
+            required:true
+        },
+        Discount:{
+            type:String,
+            required:true
+        },
+        StockQuality:{
+            type:String,
+            required:true
+        },
+        Availability:{
+            type:String,
+            required:true
+        },
+        images:{
+            type:Image,
+            required:true
+        }
+},{timestamps:true})
+const Product=mongoose.model("product",productSchema)
+export default Product
