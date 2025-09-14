c9462777a193372e96917c719d66b830851de5a8
anus26 <anusraza268@gmail.com>
2025-09-14 01:59:13 -0700

    image backend

--- src/Controllers/Pro.Controllers.js
+++ src/Controllers/Pro.Controllers.js
@@ -1,8 +1,25 @@
 import Product from "../models/Pro.models.js"
+import { v2 as cloudinary } from "cloudinary";
+
+// Cloudinary config ek hi jagah likh do (repeat na karo)
+cloudinary.config({
+  cloud_name: "dhbtwb02a",
+  api_key: "715817427384168",
+  api_secret: "l0-HfCwuRLJjh1dLyoeBId07D18",
+});
 
 const productadd=async(req,res)=>{
-const {ProductName,Category,Brand,color,Weight,Length,Width,Description,Price,StockQuality,StockQuantity,Discount,Availability}=req.body
-try {
+    const {ProductName,Category,Brand,color,Weight,Length,Width,Description,Price,StockQuality,StockQuantity,Discount,Availability}=req.body
+    
+    try {
+
+
+           if (!req.file) {
+      return res.status(400).json({ message: "Image file is required" });
+    }
+    const uploadResult = await cloudinary.uploader.upload(req.file.path, {
+    folder: "products",
+    });
     if (!ProductName ||!Category||!Brand||!color||!Weight||!Length||!Width||!Description||!Price||!StockQuality||!StockQuantity||!Discount||!Availability) return res.status(400).json({message:"All fileds are required"})
         const newproduct  =new  Product({
     ProductName,
@@ -17,11 +34,14 @@ try {
     Weight,
     Width,
     Description,
-    Availability
-
+    Availability,
+    images:uploadResult.secure_url
+    
     })
-await newproduct.save()
- return   res.status(201).json({message:"Succuessfully Add fiels",newproduct})
+    
+    await newproduct.save()
+    
+ return   res.status(201).json({message:"Succuessfully Add fiels",newproduct,})
     
 } catch (error) {
     console.error("Messages error");
