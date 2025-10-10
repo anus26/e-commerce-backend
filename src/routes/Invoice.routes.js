36f08fd373a00c32d56778ffe5e11bc569779e0a
anus26 <anusraza268@gmail.com>
2025-10-09 23:52:58 -0700

    single invoice

--- src/routes/Invoice.routes.js
+++ src/routes/Invoice.routes.js
@@ -1,7 +1,8 @@
 import express from "express"
-import {createInvoice,  getInvoice } from "../Controllers/Invoice.Controllers.js"
+import {createInvoice,  getInvoice, oneInvoice } from "../Controllers/Invoice.Controllers.js"
 
 const routerinvoice=express.Router()
 routerinvoice.post("/invoice",createInvoice)
 routerinvoice.get("/getinvoice",getInvoice)
+routerinvoice.get("/oneinvoice/:_id",oneInvoice)
 export default routerinvoice
