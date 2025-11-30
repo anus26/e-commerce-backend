4f2dbf8c044dbd9e93cb164da70ac96ffcd993c1
anus26 <anusraza268@gmail.com>
2025-11-29 23:06:34 -0800

    image

---
+++ src/multer/index.js
@@ -0,0 +1,20 @@
+import multer from "multer";
+
+
+
+const storage=multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,'./uploads')
+    },
+    filename:function(req,file,cb){
+        cb(null,Date.now()+'-'+file.originalname)
+    }
+})
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit
+});
+// const upload=multer({storage:storage})
+export default upload
