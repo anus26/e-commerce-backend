8ccd8ac66e2d2ca5e4a825b043233e3ec5cc3ac3
anus26 <anusraza268@gmail.com>
2025-12-21 20:52:05 -0800

    modified

--- src/models/Pro.models.js
+++ src/models/Pro.models.js
@@ -58,7 +58,7 @@ const productSchema=new mongoose.Schema({
             type:Boolean,
             required:true
         },
-        images:{
+        image:{
             type:[String],
             required:true
         }
